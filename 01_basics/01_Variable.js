9db6dcf168411d18ec03d87d825bce4de4791688
Arivndgarasiya <109308547+Arivndgarasiya@users.noreply.github.com>
2024-09-11 09:02:53 +0000

    Save the Changes

---
+++ 01_basics/01_Variable.js
@@ -0,0 +1,13 @@
+const accountId=144553;
+let accountEmail="Arv@gmail.com";
+var accountPassword="12345";
+
+accountCity="Jaipur";
+
+// accountId=2;//Const is Not Allows to Changes
+accountEmail="Ae@gmail.com";
+accountPassword="1234";
+// console.log(accountId);
+accountCity="Bangloru";
+
+console.table([accountId,accountEmail,accountPassword]);//Second Way to print All the element in table formate
